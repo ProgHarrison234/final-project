161c4c066bc853b9e996ec6ca70a4c1c66dc6d32
Jose-lco <ndungu642@gmail.com>
2020-08-22 13:39:12 -0700

    Added a navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
-
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
+      <Navbar />
     </div>
   );
 }
